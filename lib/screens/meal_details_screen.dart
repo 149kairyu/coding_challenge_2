d2ecb29cd3ecf6710424d4957b2f32a951d9e618
149kairyu <75082935+149kairyu@users.noreply.github.com>
2021-08-16 20:21:08 +0200

    formated lists

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import '../data/data.dart';
 
 class MealDetailsScreen extends StatelessWidget {
@@ -14,63 +15,97 @@ class MealDetailsScreen extends StatelessWidget {
         MEALS.firstWhere((meal) => meal.id == routeArguments['id']);
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('${selectedMeal.title}'),
-        ),
-        // Wichtig, damit die ganze Page Scrollbar wird
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Container(
-                  height: 300,
-                  width: double.infinity,
-                  // Image schön anpassen
-                  child:
-                      Image.network(selectedMeal.imageUrl, fit: BoxFit.cover)),
-              // Theme Title Style nutzen (Definiert in der Main)
-              Text(
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+                height: 300,
+                width: double.infinity,
+                // Image schön anpassen
+                child: Image.network(selectedMeal.imageUrl, fit: BoxFit.cover)),
+            // Theme Title Style nutzen (Definiert in der Main)
+            Container(
+              // Bisschen Spacing zwischen dem Bild und dem Text
+              // symmertric(vertical: x) nur oben und unten!
+              margin: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
                 'Zutaten',
                 style: Theme.of(context).textTheme.headline6,
               ),
-              Container(
-                height: 200,
-                width: 400,
-                child: ListView.builder(
+            ),
+            // Hier kann nicht einfach eine ListView() angewendet werden
+            // ListView nimmt "unendlich" höhe und darüber nimmt auch Column unendlich höhe
+            // ==> Deswegen muss ein Container dazwischengeschalten werden
+            // Media Query kann angewendet werden, um hier die höhen dynamisch anzupassen!
+            // Jedoch frisst das viel Zeit !
+            Container(
+              // Da die ListView unten bei zu vielen Elementen scrollbar wird
+              // ==> decorations einfügen um dies deutlich zu zeigen
+              // Borders in alle Richtungen
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10)),
+              // Margin und Padding um die "Textbox"
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 200,
+              width: 400,
+              child: ListView.builder(
                   itemCount: selectedMeal.ingredients.length,
                   itemBuilder: (context, index) => Card(
-                    color: Theme.of(context).primaryColorDark,
-                    // Padding um den Text selbst
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                      child: Text(selectedMeal.ingredients[index]),
-                    ),
-                  ),
-                ),
-              ),
-              Text(
+                        color: Theme.of(context).primaryColorDark,
+                        // Padding um den Text selbst
+                        child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 10),
+                            child: Text(selectedMeal.ingredients[index])),
+                      )),
+            ),
+            // BONUSAUFGABE
+            // CODE DUPLICATION => GEHT AUCH ÜBER DAS AUSLAGERN IN EINE METHODE
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
                 'Schritte',
                 style: Theme.of(context).textTheme.headline6,
               ),
-              // CONTAINER MIT DEFINIERTER HÖHE UND BREITE MÜSSEN HIER ANGEGEBEN WERDEN
-              // DA LIST VIEW SICH AN DIESE ANPASST [OHNE GIBTS FEHLER]
-              Container(
-                height: 200,
-                width: 400,
-                child: ListView.builder(
+            ),
+            // Hier kann nicht einfach eine ListView() angewendet werden
+            // ListView nimmt "unendlich" höhe und darüber nimmt auch Column unendlich höhe
+            // ==> Deswegen muss ein Container dazwischengeschalten werden
+            // Media Query kann angewendet werden, um hier die höhen dynamisch anzupassen!
+            // Jedoch frisst das viel Zeit !
+            Container(
+              // Da die ListView unten bei zu vielen Elementen scrollbar wird
+              // ==> decorations einfügen um dies deutlich zu zeigen
+              // Borders in alle Richtungen
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10)),
+              // Margin und Padding um die "Textbox"
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 200,
+              width: 400,
+              child: ListView.builder(
                   itemCount: selectedMeal.steps.length,
                   itemBuilder: (context, index) => Card(
-                    color: Theme.of(context).primaryColorDark,
-                    // Padding um den Text selbst
-                    child: Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        child: Text(selectedMeal.steps[index])),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ));
+                        color: Theme.of(context).primaryColorDark,
+                        // Padding um den Text selbst
+                        child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 10),
+                            child: Text(selectedMeal.steps[index])),
+                      )),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
